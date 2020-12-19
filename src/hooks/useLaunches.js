f17e356c7ba7c47d6a4254e64dfbbda883e5dae5
Francisco <fco.cordero98@gmail.com>
2020-12-19 19:58:11 -0300

    adding functions

--- src/hooks/useLaunches.js
+++ src/hooks/useLaunches.js
@@ -13,22 +13,51 @@ const splitLaunchesOnPages = (launches) => {
 };
 
 const useLaunches = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   const [launches, setLaunches] = useState([]);
+  const [launchesStore, setLaunchesStore] = useState([]);
   const { pages, maxPages } = splitLaunchesOnPages(launches);
 
   const getLaunchesCallback = useCallback(async () => {
     const data = await getLaunches();
     setLaunches(data);
+    setLaunchesStore(data);
   }, []);
 
   useEffect(() => {
     getLaunchesCallback();
   }, [getLaunchesCallback]);
 
+  const nextPage = () => {
+    const nextPage = Math.min(currentPage + 1, maxPages - 1);
+    setCurrentPage(nextPage);
+  };
+
+  const previousPage = () => {
+    const previousPage = Math.max(currentPage - 1, 0);
+    setCurrentPage(previousPage);
+  };
+
+  const onSearch = (launchName = "") => {
+    if (launchName === "") {
+      setLaunches(launchesStore);
+    } else {
+      const result = launches.filter((launch) =>
+        launch.name?.toUpperCase().includes(launchName?.toUpperCase())
+      );
+      setLaunches(result);
+    }
+    setCurrentPage(0);
+  };
+
   return {
     launches,
     pages,
     maxPages,
+    onSearch,
+    nextPage,
+    previousPage,
+    currentPage,
   };
 };
 
